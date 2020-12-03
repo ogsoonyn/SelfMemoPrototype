6088e6d3ff11c587009192918effef23e0d5f326
ynaka <ynakamura027@gmail.com>
2020-12-03 17:59:49 +0900

    キーボードショートカットを追加
    Escでアプリ終了など。
    あとデフォルトでTextBoxをフォーカスするようにした

---
+++ SelfMemoPrototype/Behavior/ApplicationCommandBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace SelfMemoPrototype.Behavior
+{
+    class ApplicationCommandBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            CommandBinding CloseCommandBinding = new CommandBinding(
+                ApplicationCommands.Close,
+                CloseCommandExecuted,
+                CloseCommandCanExecute);
+            AssociatedObject.CommandBindings.Add(CloseCommandBinding);
+        }
+
+        private void CloseCommandExecuted(object target, ExecutedRoutedEventArgs e)
+        {
+            AssociatedObject.Close();
+            e.Handled = true;
+        }
+
+        private void CloseCommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+    }
+}
